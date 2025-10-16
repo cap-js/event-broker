3798c60f3489fb1c1bc8b87594a8d13d8784c80f
Tongwei Zhang <tongwei.zhang@sap.com>
2025-10-16 15:24:06 -0400

    resolve PR comments

--- test/event-broker-ias-single-tenant/srv/ownSrv.js
+++ test/event-broker-ias-single-tenant/srv/ownSrv.js
@@ -1,22 +1,15 @@
 const cds = require('@sap/cds');
 
-module.exports = async () => {
+module.exports = async (srv) => {
 
   const messaging = await cds.connect.to('messaging')
   messaging.on('someEvent', () => { })
 
-  setInterval(async () => {
+  srv.on('triggerEvent', async () => {
     let eventType = "ns.fh.employee.feedbackCollector-create.v1"
 
-    try {
-      await messaging.emit(eventType, {
-        data: "testdata",
-      });
-
-      console.log(`SUCCESS: Event ${eventType} emitted successfully`);
-    } catch (error) {
-      console.log(`FAILURE: Failed to emit event ${eventType}`);
-      console.log(`Error details:`, error);
-    }
-  }, 5000);
+    await messaging.emit(eventType, {
+      data: "testdata",
+    });
+  })
 }
